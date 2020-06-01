290b4de049837c8a947833bf00be81bf304d8b1d
Karena Yan <kyan@flyingfish.princeton.edu>
2020-06-01 17:10:24 -0400

    first commit

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Create a github_repo from the command line...so cool (more here: https://developer.github.com/v3/repos/#create)
 # I should probably implement some check if the creation was successful
-curl -u 'kKYan19' https://api.github.com/user/repos -d '{"name":"pei", "private":false}'
+curl -u 'KYan19' https://api.github.com/user/repos -d '{"name":"pei", "private":false}'
 
 # Link local repository to git
 git init
 git add *
 git add .gitignore .stickler.yml .travis.yml
 git commit -m 'first commit'
-git remote add origin git@github.com:kKYan19/pei.git
+git remote add origin git@github.com:KYan19/pei.git
 git push -u origin master
