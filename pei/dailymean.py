027282f68fffd6a6fd2d149dd542e9748e87d7f0
Karena Yan <kyan@dogfish.Princeton.EDU>
2020-07-05 14:15:05 -0400

    Data processing for daily mean WBT

---
+++ pei/dailymean.py
@@ -0,0 +1,17 @@
+import xarray as xr
+
+# List of ensembles members to process
+ensembles = ['002','003','004','005']
+
+for ens in ensembles:
+    # Path to data files for this ensemble member
+    paths = '/local/ss23/CESM2_LE/ATM/RCP85/WBT/b.e21.*.f09_g17.LE2-1231.' + ens + '.clm2.h7.WBT.*.nc'
+    
+    # Concat along time dimension
+    ds = xr.open_mfdataset(paths,combine='by_coords',coords=['time'],chunks={'time':1095})['WBT']
+    
+    # Calculate daily mean
+    ds_max = ds.resample(time='1D').mean()
+    
+    # Save as new data file
+    ds_max.to_netcdf('../data/processed/CESM2/WBTdailymean/WBTens'+ens+'.nc')
